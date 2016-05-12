67f053d3d91fd79205118538f9b9c0cb054e749a
wangaq <1247787084@qq.com>
2016-05-12 09:35:30 +0800

    新增demo1

--- mysql_async.js
+++ mysql_async.js
@@ -1,7 +1,9 @@
 /**
  * Created by WANGAQ on 2016/4/19.
+ * async 异步控制demo
  */
 var mysql = require('mysql');
+var async = require('async');
 
 var conn = mysql.createConnection({
     host: 'localhost',
@@ -11,24 +13,29 @@ var conn = mysql.createConnection({
     port: '3306'
 });
 conn.connect();
-var date = new Date();
-var insertSQL = 'insert into user(name) values("xiaom"),("xiaomi"),("hongm")';
-var selectSQL = 'select * from user';
-var deleteSQL = 'delete from user where id=1';
-var updateSQL = 'UPDATE USER SET NAME="xiaomi" WHERE ID=1';
+
+var sqls = {
+    'insertSQL' : 'insert into user(name) values("xiaom"),("xiaomi"),("hongm")',
+    'selectSQL' : 'select * from user',
+    'deleteSQL' : 'delete from user where id=1',
+    'updateSQL' : 'UPDATE USER SET NAME="xiaomi" WHERE ID=1'
+}
+
+var tasks = ['insertSQL','selectSQL','deleteSQL','updateSQL']
+
+//async 并行执行
+async.eachSeries(tasks,function(item,callback) {
+    console.log('item : ' + sqls[item]);
+    conn.query(sqls[item], function(err,res) {
+        console.log(res);
+        callback(err, res);
+    });
+}, function(err) {
+    console.log('err : ' + err);
+});
 
 /*conn.query(selectSQL,function(err,info) {
     if(err) console.log('错误：%s',err);
     console.log('sql result ==>');
     console.log(info);
 });*/
-
-conn.query(selectSQL,function(err,info) {
-    if(err) console.log('错误：%s',err);
-    for(var i in info) {
-        console.log('sql result ==>');
-        console.log(info[i].Id);
-    }
-});
-
-conn.end();
